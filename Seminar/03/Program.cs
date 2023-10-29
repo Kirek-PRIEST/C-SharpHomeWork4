218afdf6f2d85f578189138436f7cf6fb1397919
Макс Просто <kirek@mail.ru>
2023-10-29 12:11:55 +0500

    занятие 05

--- Seminar/03/Program.cs
+++ Seminar/03/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+int ReadIn(string msg)
+{
+    Console.WriteLine(msg);
+    string b = Console.ReadLine();
+    return int.Parse(b);
+}
+
+int searchFactorial(int incoming)
+{
+    int result = 1;
+    int temp = 1;
+    while (incoming > 0)
+    {
+        result = result * temp;
+        temp++;
+        incoming--;
+    }
+    return result;
+}
+
+Console.WriteLine(searchFactorial(ReadIn("Введите число")));
---
+++ Seminar/05/Program.cs
@@ -0,0 +1,48 @@
+﻿int[] CreateMassive(int length) //Метод для создания массива определённой длины
+{
+    int[] array = new int[length];//создаём массив заданной длинны
+    int i = 0;
+    while (i < array.Length) //счётчик менее длины массива
+    {
+        array[i] = new Random().Next(1, 10); //заполнение поля массива случайным числом
+        i++;
+    }
+    return array; //возврат массва из функции
+}
+void printMassive(int[] massive) //метод ля вывода массива
+{
+    foreach (var item in massive)/*оператор для вывода массива (каждый элемент в массиве massive называется item (в данном случае) )
+                                    и для каждого элемента выполняется описаное ниже действие, пока не закончится массив)*/
+    {
+        Console.Write(item + " ");
+
+    }
+    Console.WriteLine();
+}
+
+void NewArrayMin(int[] array)
+{
+    int lenght = array.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        for (int j = 0; j < lenght-1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                int temp = array[j+1];
+                array[j+1] = array[j];
+                array[j] = temp;
+
+                printMassive(array);
+            }
+
+        }
+
+
+
+    }
+}
+int[] massive = CreateMassive(8);
+printMassive(massive);
+Console.WriteLine();
+NewArrayMin(massive);
