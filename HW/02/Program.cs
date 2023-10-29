baef58c4dde8909dd533bdb3a3bbc7e691e7bc4c
Макс Просто <kirek@mail.ru>
2023-10-29 23:00:56 +0500

    ДЗ 2

---
+++ HW/02/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadIn(string msg)
+{
+    Console.WriteLine(msg);
+    string a = Console.ReadLine();
+    return int.Parse(a);
+}
+
+int Sum(int num)
+{
+    int temp = num;
+    int result = 0;
+    for (int i = temp; i > 0; i--)
+    {
+        result = result + temp%10;
+        temp = temp / 10;
+    }
+    return result;
+}
+int Pluas(int num)
+{
+    if (num < 0)
+    {
+        int result = num * -1;
+        return result;
+    }
+    return num;
+}
+int chislo = Pluas(ReadIn("Введите число:"));
+Console.WriteLine(Sum(chislo));
+
