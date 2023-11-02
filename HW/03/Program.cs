e4b79b63f56d82a47d90b17b4e10652289d00218
Макс Просто <kirek@mail.ru>
2023-11-03 01:12:22 +0500

    ДЗ, семинар

--- HW/03/Program.cs
+++ HW/03/Program.cs
@@ -2,3 +2,35 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
+int ReadIn(string msg)
+{
+    Console.WriteLine(msg);
+    string a = Console.ReadLine();
+    return int.Parse(a);
+}
+int[] CreateMassive(int length) //Метод для создания массива определённой длины
+{
+    int[] array = new int[length];//создаём массив заданной длинны
+    int i = 0;
+    while (i < array.Length) //счётчик менее длины массива
+    {
+        array[i] = new Random().Next(10); //заполнение поля массива случайным числом
+        i++;
+    }
+    return array; //возврат массва из функции
+}
+void printMassive(int[] massive) //метод ля вывода массива
+{
+    Console.Write("[");
+    int i = 0;
+    while (i < massive.Length-1)
+    {
+        
+        Console.Write( + massive[i] + ", ");
+        i++;
+    }
+    Console.Write(massive[i]);
+    Console.WriteLine("]");
+}
+
+printMassive(CreateMassive(ReadIn("Введите длинну массива")));
